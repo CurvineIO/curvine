29749e6ec7729e9d1b1e0158871a1226d8842fd4
John <hellojlong@163.com>
2026-07-06 19:20:13 +0800

    fix(worker): return recoverable errors instead of panicking (#988)

--- curvine-server/src/test/mini_cluster.rs
+++ curvine-server/src/test/mini_cluster.rs
@@ -122,7 +122,12 @@ impl MiniCluster {
     pub fn start_worker(&self) {
         for conf in &self.worker_conf {
             let worker = Worker::with_conf(conf.clone()).unwrap();
-            thread::spawn(move || worker.block_on_start());
+            thread::spawn(move || {
+                if let Err(e) = worker.block_on_start() {
+                    log::error!("mini cluster worker failed to start: {}", e);
+                    std::process::abort();
+                }
+            });
         }
     }
 
