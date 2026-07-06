7a28e7e5dbd69d183d1f5f8e27ead04a86ae5679
barry <lzjqsdd@gmail.com>
2026-07-06 15:05:14 +0800

    fix(orpc): skip logger init when tracing subscriber already set (#967)
    When curvine-libsdk is embedded in a host that already installed a global
    tracing subscriber, Logger::init no longer panics; it defers to the host
    logging configuration instead.

---
+++ orpc/tests/logger_test.rs
@@ -0,0 +1,25 @@
+// Copyright 2025 OPPO.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use orpc::common::{LogConf, Logger};
+
+#[test]
+fn logger_init_does_not_panic_when_global_subscriber_already_set() {
+    let _ = tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::ERROR)
+        .with_test_writer()
+        .try_init();
+
+    Logger::init(LogConf::default());
+}
