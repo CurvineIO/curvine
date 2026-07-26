344dc73c4efc7a3c441cbe688d6725290f4ff6bc
barry <lzjqsdd@gmail.com>
2026-07-26 11:29:04 +0800

    refactor(sdk): split libsdk core and language bindings

--- curvine-libsdk-java/src/java/java_filesystem.rs
+++ curvine-libsdk-java/src/java/java_filesystem.rs
@@ -12,16 +12,15 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use crate::core::job;
 use crate::java::JavaUtils;
 use crate::{FilesystemConf, LibFilesystem, LibFsReader, LibFsWriter};
-use curvine_common::error::FsError;
-use curvine_common::proto::{
+use curvine_error::{FsError, FsResult};
+use curvine_fs_api::proto::{
     GetJobStatusResponse, GetMountTableResponse, MountOptionsProto, SubmitJobResponse,
 };
-use curvine_common::state::LoadJobCommand;
-use curvine_common::utils::ProtoUtils;
-use curvine_common::FsResult;
+use curvine_fs_api::state::LoadJobCommand;
+use curvine_fs_api::utils::ProtoUtils;
+use curvine_sdk_core::blocking_job as job;
 use jni::objects::{JByteArray, JString};
 use jni::sys::{jarray, jboolean, jstring};
 use jni::JNIEnv;
@@ -32,7 +31,7 @@ pub struct JavaFilesystem {
     inner: LibFilesystem,
 }
 
-fn decode_mount_options(bytes: &[u8]) -> FsResult<curvine_common::state::MountOptions> {
+fn decode_mount_options(bytes: &[u8]) -> FsResult<curvine_fs_api::state::MountOptions> {
     if bytes.is_empty() {
         return err_box!("mount options cannot be empty");
     }
