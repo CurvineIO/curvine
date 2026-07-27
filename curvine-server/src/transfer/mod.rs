1ac8682e425da55abd053309710a4e68e05be94d
jlon <itjlon@gmail.com>
2026-07-27 04:18:45 +0000

    feat(transfer): add MySQL transfer store

--- curvine-server/src/transfer/mod.rs
+++ curvine-server/src/transfer/mod.rs
@@ -20,3 +20,6 @@ pub use self::memory_store::MemoryTransferStore;
 
 mod sqlite_store;
 pub use self::sqlite_store::SqliteTransferStore;
+
+mod mysql_store;
+pub use self::mysql_store::MysqlTransferStore;
